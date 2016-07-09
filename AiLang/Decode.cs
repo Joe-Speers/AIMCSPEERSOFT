053ad8f527103a1332d12d9f513ea94f6c0dc7cf
josep <josep@DESKTOP-HNPTHN8>
2016-07-09 21:04:03 +0100

    Add project files.

---
+++ AiLang/Decode.cs
@@ -0,0 +1,320 @@
+﻿using System;
+
+public class Decode
+{
+    int i=0;
+    string code;
+    string st = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    class Memory
+    {
+        public string[] s = new string[100];
+        public int[] i = new int[100];
+        public bool[] b = new bool[100];
+    }
+    Memory M;
+	public Decode(string inputcode)
+	{
+        code = inputcode;
+        M = new Memory();
+        instruct();
+        
+	}
+
+    void instruct()
+    {
+        while (true)
+        {
+            if (code.Length < i+1)
+            {
+                return;
+            }
+            char c = code[i];
+
+            if (c == '#')
+            {
+                Set();
+                i++;
+                continue;
+            }
+            if (c == '~')
+            {
+                i++;
+                System.Threading.Thread.Sleep(readI());
+                i = i + 2;
+                continue;
+            }
+            if (c == '$')
+            {
+                i++;
+                object output=null;
+                switch (GetType())
+                {
+                    case 1:
+                        output = readI();
+                        break;
+                    case 2:
+                        output = readS();
+                        break;
+                    case 3:
+                        output = readB();
+                        break;
+                }
+                i = i + 2;
+                Console.WriteLine(output);
+                continue;
+
+            }
+            //loops
+            if (c == '[')
+            {
+                i++;
+                if (code[i] == '(')
+                {
+                    i++;
+                    if (readB())
+                    {
+                        i = i + 2;
+                        instruct();
+                        continue;
+                    }
+                    else
+                    {
+                        while (code[i] != ']')
+                        {
+                            i++;
+                        }
+                        i=i+2;
+                        if (code[i] == '[' && code[i + 1] != '(')
+                        {
+                            instruct();
+                            continue;
+                        }
+                    }
+                }
+                else
+                {
+                    while (code[i] != ']')
+                    {
+                        i++;
+                    }
+                    i++;
+                    continue;
+                }
+            }
+            if (c == ']')
+            {
+                i++;
+                return;
+            }
+        }
+    }
+
+    //functions
+    void Set()
+    {
+        i++;
+        int read = readI();
+        i++;
+        int type = GetType();
+        object value;
+        switch (type)
+        {
+            case 1:
+                M.i[read]=readI();
+                break;
+            case 2:
+                M.s[read] = readS();
+                break;
+            case 3:
+                M.b[read] = readB();
+                break;
+        }
+        i++;
+
+    }
+
+    //reading
+    int GetType()
+    {
+        if (code[i] == '<')
+        {
+            return 1;//int
+        }
+        if (code[i] == '"')
+        {
+            return 2;//string
+        }
+        if (code[i] == '=')
+        {
+            return 3;//bool
+        }
+        return 0;
+    }
+    string readS()
+    {
+        i++;
+        char c = code[i];
+        if (c == '?')
+        {
+            i++;
+            string v=M.s[readI()];
+            i++;
+            return v;
+        }
+        if (c == '!')
+        {
+            i++;
+            return st[new Random().Next(st.Length)].ToString();
+        }
+        if(st.Contains(c.ToString()))
+        {
+            string v ="";
+            while (st.Contains(code[i].ToString()))
+            {
+                v = v + code[i];
+                i++;
+            }
+            return v;
+        }
+        if (c == '+')
+        {
+            string v = "";
+            i++;
+            switch (GetType())
+            {
+                case 1:
+                    v = v + readI().ToString();
+                    break;
+                case 2:
+                    v = v + readS();
+                    break;
+                case 3:
+                    v = v + readB().ToString();
+                    break;
+            }
+            i++;
+            switch (GetType())
+            {
+                case 1:
+                    v = v + readI().ToString();
+                    break;
+                case 2:
+                    v = v + readS();
+                    break;
+                case 3:
+                    v = v + readB().ToString();
+                    break;
+            }
+            i++;
+            return v;
+        }
+        return "X";
+    }
+    int readI()
+    {
+        i++;
+        char c = code[i];
+        if (c == '?')
+        {
+            i++;
+            int v = M.i[readI()];
+            i++;
+            return v;
+        }
+        if (c == '!')
+        {
+            i++;
+            int v1 = readI();
+            i++;
+            int v2 = readI();
+            i++;
+            return new Random().Next(v1,v2);
+        }
+        if (char.IsDigit(c))
+        {
+            string v = "";
+            while (char.IsDigit(code[i]))
+            {
+                v = v + code[i].ToString();
+                i++;
+            }
+            return Convert.ToInt16(v);
+        }
+        if (c == '+')
+        {
+            int v = 0;
+            i++;
+            v = readI();
+            i++;
+            v = v + readI();
+            i++;
+            return v;
+        }
+        if (c == '*')
+        {
+            int v = 0;
+            i++;
+            v = readI();
+            i++;
+            v = v * readI();
+            i++;
+            return v;
+        }
+        if (c == '/')
+        {
+            int v = 0;
+            i++;
+            v = readI();
+            i++;
+            v = v / readI();
+            i++;
+            return v;
+        }
+        if (c == '-')
+        {
+            int v = 0;
+            i++;
+            v = readI();
+            i++;
+            v = v - readI();
+            i++;
+            return v;
+        }
+        return 0;
+    }
+    bool readB()
+    {
+        i++;
+        char c = code[i];
+        if (c == '?')
+        {
+            i++;
+            bool v = M.b[readI()];
+            i++;
+            return v;
+        }
+        if (c == '!')
+        {
+            i++;
+            if (new Random().Next(2) == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        if (c=='0')
+        {
+            i++;
+            return false;
+        }
+        if (c == '1')
+        {
+            i++;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ AiLang/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiLang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+            Decode D = new Decode("#<5><!<0><50>>##<6><9>##<7><*<?<5>><?<6>>>##<8>\"+\"Result\"<?<7>>\"#~<1000>~$\"?<8>\"$~<500>~$\"+\"XWas\"<?<5>>\"$~<500>~$\"+\"YWas\"<?<6>>\"$");
+            while (true)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+    }
+    //#<5><!<0><50>>##<6><9>##<7><*<?<5>><?<6>>>##<8>\"+\"Result\"<?<7>>\"#$\"?<8>\"$$\"+\"XWas\"<?<5>>\"$$\"+\"YWas\"<?<6>>\"$
+}
